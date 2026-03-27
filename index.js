e530bbf0dffbf2dbb3d301d1fe771f5c4c2054aa
psielska9560-blip <psielska9560@gmail.com>
2026-03-27 14:15:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,13 +10,32 @@ app.get("/", async (req, res) => {
       executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
       args: [
         "--no-sandbox",
-        "--disable-setuid-sandbox"
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+        "--disable-gpu",
+        "--disable-software-rasterizer",
+        "--disable-background-networking",
+        "--disable-background-timer-throttling",
+        "--disable-backgrounding-occluded-windows",
+        "--disable-breakpad",
+        "--disable-component-extensions-with-background-pages",
+        "--disable-default-apps",
+        "--disable-extensions",
+        "--disable-features=TranslateUI",
+        "--disable-ipc-flooding-protection",
+        "--disable-renderer-backgrounding",
+        "--memory-pressure-off",
+        "--single-process",
+        "--no-zygote",
+        "--no-first-run",
+        "--no-default-browser-check"
       ]
     });
 
     const page = await browser.newPage();
+
     await page.goto("https://www.olx.pl/uslugi/q-elektryk-dolny-slask/", {
-      waitUntil: "networkidle2",
+      waitUntil: "domcontentloaded",
       timeout: 60000
     });
 
