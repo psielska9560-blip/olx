b72a8e68ed11d5038152b3981f7d3ff1ffb390fa
psielska9560-blip <psielska9560@gmail.com>
2026-03-27 10:36:44 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import puppeteer from "puppeteer";
+
+const app = express();
+
+app.get("/", async (req, res) => {
+  try {
+    const browser = await puppeteer.launch({
+      headless: "new",
+      args: ["--no-sandbox", "--disable-setuid-sandbox"]
+    });
+
+    const page = await browser.newPage();
+    await page.goto("https://www.olx.pl/uslugi/q-elektryk-dolny-slask/", {
+      waitUntil: "networkidle2",
+      timeout: 60000
+    });
+
+    const html = await page.content();
+    await browser.close();
+
+    res.send(html);
+  } catch (err) {
+    res.send("Błąd: " + err.message);
+  }
+});
+
+app.listen(3000, () => console.log("Scraper działa na porcie 3000"));
